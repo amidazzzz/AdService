9e9583b9dc45c236ce76b62c390d3b30b4c14211
envy <cain.wells@mail.ru>
2024-01-12 17:00:52 +0300

    Initial commit

---
+++ src/main/java/ru/amida/advertservice/model/Customer.java
@@ -0,0 +1,9 @@
+package ru.amida.advertservice.model;
+
+public class Customer {
+    private Long id;
+    @
+    private String name;
+    Set<Advert> adverts,
+
+}
---
+++ src/test/java/ru/amida/advertservice/AdvertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.amida.advertservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
