1c86a33c8a7dc89c74e6f26ac48381909d14466c
envy <cain.wells@mail.ru>
2024-01-14 16:44:56 +0300

    Added security config

--- src/main/java/ru/amida/advertservice/config/SecurityConfig.java
+++ src/main/java/ru/amida/advertservice/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package ru.amida.advertservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +16,18 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/register").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                );
 
+        return http.build();
+    }
 }
